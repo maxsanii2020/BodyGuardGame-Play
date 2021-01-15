ed84927588b81c4f150aa14bea97a429c716bc09
maxsanii2020 <58756642+maxsanii2020@users.noreply.github.com>
2021-01-14 23:48:50 -0800

    updated game loading text

--- states/index.js
+++ states/index.js
@@ -122,8 +122,8 @@ preload: function() {
     //game.load.image('loading_bar', 'assets/images/loading_bar.png');
     //game.load.image('mission-accomplished', 'assets/images/missionAccomplished.png');
     //var loadingBar = game.add.sprite(300, 300, 'mission-accomplished');
-
-    var sampleTextt = game.add.text(345, 300, "Helloooo");
+    var styleTest = { font: "40px Arial", fill: "#FFFFFF", align: "center", backgroundColor:null, fontWeight: "bold"};
+    var sampleTextt = game.add.text(this.game.world.centerX-200, this.game.world.centerY-100, 'Loading...', styleTest);
 
     /*this.loadingBar = game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loading_bar');
     this.loadingBar.anchor.setTo(0.5);
